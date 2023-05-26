7c95ea8b7f318f3b0a890f2d60850d2c07d7e826
Crystal <crwainstock@gmail.com>
2023-05-26 12:53:18 +0200

    added navbar styling with tailwind, yay!

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,13 +3,35 @@ import { Link, NavLink } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <header className="bg-slate-300">
-      <nav className="container mx-auto flex justify-between">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/projects">Projects</NavLink>
-        <NavLink to="/blog">Blog</NavLink>
-        <NavLink to="/about">About</NavLink>
-      </nav>
+    <header className="bg-white shadow dark:bg-gray-800">
+      <div className="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+        <nav className="flex">
+          <NavLink
+            to="/"
+            className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-red-400 mx-1.5 sm:mx-6"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/projects"
+            className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-red-400 mx-1.5 sm:mx-6"
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            to="/blog"
+            className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-red-400 mx-1.5 sm:mx-6"
+          >
+            Blog
+          </NavLink>
+          <NavLink
+            to="/about"
+            className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-red-400 mx-1.5 sm:mx-6"
+          >
+            About
+          </NavLink>
+        </nav>
+      </div>
     </header>
   );
 }
