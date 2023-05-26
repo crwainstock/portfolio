ca13d73f0f5c439dcfdf4359a5fe97a87ffd642b
Crystal <crwainstock@gmail.com>
2023-05-26 12:17:54 +0200

    trying to get tailwind classes to render, showing up in elements in browser, but not changing render

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html"],
+  // content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
 };
+
+// Having trouble getting tailwind to render -- showing up in elements in Browser, but not in render
