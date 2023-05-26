34080be9235548bb75d58d0f2ea706b4746f654b
Crystal <crwainstock@gmail.com>
2023-05-26 11:58:38 +0200

    add header to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,11 +3,13 @@ import { Link, NavLink } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/projects">Projects</NavLink>
-      <NavLink to="/blog">Blog</NavLink>
-    </nav>
+    <header>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/projects">Projects</NavLink>
+        <NavLink to="/blog">Blog</NavLink>
+      </nav>
+    </header>
   );
 }
