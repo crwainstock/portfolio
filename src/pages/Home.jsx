7b2e10bf9d858fa81cbceddb32dd520ca353292a
Crystal <crwainstock@gmail.com>
2023-05-27 09:52:25 +0200

    trying to get home page styling in order, but text won't overlay image properly yet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import image from "../oleg-laptev-7jQh3EiS8Bs-unsplash.jpg";
 
 export default function Home() {
   return (
-    <div>
-      <h3>This is the home page</h3>
-    </div>
+    <main>
+      <img
+        src={image}
+        alt="white building walls intersecting"
+        className="absolute object-contain w-full h-full"
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-green-400 text-6xl font-bold leading-none lg:leading-snug home-name">
+          Hi, I'm Crystal.
+        </h1>
+      </section>
+    </main>
   );
 }
