ae54916001653f426a6bda73b6a2d497b50a0a52
Crystal <crwainstock@gmail.com>
2023-05-28 14:27:19 +0200

    import useState, useEffect, sanityClient in blog posts page

--- src/pages/BlogPosts.jsx
+++ src/pages/BlogPosts.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import sanityClient from "../client";
 
 export default function BlogPosts() {
+  const [postData, setPostData] = useState();
+
   return (
     <div>
       <h3>This is the blog posts page</h3>
