a1c801e1ee60ff37e3b4aece49ece31409613f5f
Crystal <crwainstock@gmail.com>
2023-05-28 14:48:08 +0200

    blog posts page set up without content in sanity

--- src/pages/BlogPosts.jsx
+++ src/pages/BlogPosts.jsx
@@ -29,19 +29,31 @@ export default function BlogPosts() {
       <section className="container mx-auto">
         <h1 className="text-5xl flex justify-center">Journal</h1>
         <h2 className="text-lg text-gray-600 flex justify-center mb-12">
-          Thoughts about coding, learning new things, and life.
+          Thoughts about coding & learning new things.
         </h2>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-          <article>
-            <Link to="">
-              <span>
-                <img />
-                <span>
-                  <h3></h3>
-                </span>
-              </span>
-            </Link>
-          </article>
+          {postData &&
+            postData.map((post, index) => (
+              <article>
+                <Link to={"/post/" + post.slug.current} key={post.slug.current}>
+                  <span
+                    className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400"
+                    key={index}
+                  >
+                    <img
+                      src={post.mainImage.asset.url}
+                      alt={post.mainImage.alt}
+                      className="w-full h-full rounded-r object-cover absolute"
+                    />
+                    <span className="relative h-full flex justify-end items-end pr-4 pb-4">
+                      <h3 className="text-gray-800 text-lg font-bold px-3 py-4 bg-red-700 bg-opacity-75 rounded">
+                        {post.title}
+                      </h3>
+                    </span>
+                  </span>
+                </Link>
+              </article>
+            ))}
         </div>
       </section>
     </main>
