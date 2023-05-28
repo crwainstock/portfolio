71713716de63b5bd6bbd675ffe0163907c1eca64
Crystal <crwainstock@gmail.com>
2023-05-28 15:20:44 +0200

    getting error caught Error: Configuration must contain

--- src/client.js
+++ src/client.js
@@ -1,13 +1,11 @@
 import { SanityClient } from "@sanity/client";
+// import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+// dotenv.config();
+// import express from "express";
 
 const client = new SanityClient({
-  projectId: process.env.projectId,
+  projectId: "6n6s9lrw",
   dataset: "production",
 });
 
 export default client;
-
-// export default SanityClient({
-//   projectId: "6n6s9lrw",
-//   dataset: "production",
-// });
