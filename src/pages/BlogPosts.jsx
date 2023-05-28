521f60ebfd9608693be5783819f9685ace1c581e
Crystal <crwainstock@gmail.com>
2023-05-28 14:37:05 +0200

    put in jsx shell and some styling in blog posts page, fetch to sanity

--- src/pages/BlogPosts.jsx
+++ src/pages/BlogPosts.jsx
@@ -3,11 +3,47 @@ import { Link } from "react-router-dom";
 import sanityClient from "../client";
 
 export default function BlogPosts() {
-  const [postData, setPostData] = useState();
+  const [postData, setPostData] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "post"]{
+      title,
+      slug,
+      mainImage{
+        asset->{
+          _id,
+          url
+        },
+        alt
+      }
+    }`
+      )
+      .then((data) => setPostData(data))
+      .catch(console.error);
+  }, []);
 
   return (
-    <div>
-      <h3>This is the blog posts page</h3>
-    </div>
+    <main className="bg-green-100 min-h-screen p-12">
+      <section className="container mx-auto">
+        <h1 className="text-5xl flex justify-center">Journal</h1>
+        <h2 className="text-lg text-gray-600 flex justify-center mb-12">
+          Thoughts about coding, learning new things, and life.
+        </h2>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <article>
+            <Link to="">
+              <span>
+                <img />
+                <span>
+                  <h3></h3>
+                </span>
+              </span>
+            </Link>
+          </article>
+        </div>
+      </section>
+    </main>
   );
 }
