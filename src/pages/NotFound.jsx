8638adcc3cc912aeabf5d267234963bd248f66b8
Crystal <crwainstock@gmail.com>
2023-06-02 14:16:47 +0200

    updating not found page styling

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,11 +3,15 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div className="not-found-container">
-      <h1>Sorry, the page you were looking for was not found.</h1>
-      <Link to="/" className="link-button">
-        Return to home
-      </Link>
+    <div className="mt-20">
+      <h1 className="text-yellow-400 flex justify-center font-bold lg:leading-snug sm:text-2xl mx-auto p-10">
+        Sorry, the page you were looking for was not found.
+      </h1>
+      <button className="rounded-md bg-zinc-300 hover:bg-zinc-200 p-3 flex mx-auto">
+        <Link to="/" className="link-button">
+          Return to home
+        </Link>
+      </button>
     </div>
   );
 }
