e3479fd3fae454d9e21e7f5ba5d0afe6661a598f
Crystal <crwainstock@gmail.com>
2023-06-05 09:18:51 +0200

    reinstalled tailwind, prose class not accessible with cdn?

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  // The following stuff was recommended as a solution for getting tailwind to render appropriately in the deployed version. Didn't work here.
-  // publicDir: `public`,
-  // build: {
-  //   outDir: `../dist`,
-  // },
   server: {
     port: 3000,
     proxy: {
