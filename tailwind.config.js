188368514f494c1323ebbf7debb97fedeb296d86
Crystal <crwainstock@gmail.com>
2023-06-05 08:46:51 +0200

    added tailwind cdn to html file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,7 @@ export default {
   // content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   // content: ["./src/**/*.{html,js}"],
-  // Idk. One solution to rendering tailwind in deployed version is to include this purge below. Not working for me, though.
-  purge: ["./src/components/**/*.{js,jsx}", "./public/index.html"],
+
   safelist: [],
   theme: {
     extend: {},
