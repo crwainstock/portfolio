016093b2e668c7967e2f8b571317379d0f9e35f2
Crystal <crwainstock@gmail.com>
2023-06-05 08:39:56 +0200

    added purge to tailwind config per suggestion online

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   // content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   // content: ["./src/**/*.{html,js}"],
+  purge: ["./src/components/**/*.{js,jsx}", "./public/index.html"],
   safelist: [],
   theme: {
     extend: {},
